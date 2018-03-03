e08ba17287f5eb177459aa48b94a949894c2e372
罗奕斐 <gdutyifei@163.com>
2018-03-03 22:21:28 +0800

    产品详情

--- page/view/productDetail/productDetail.js
+++ page/view/productDetail/productDetail.js
@@ -1,5 +1,6 @@
 // page/view/productDetail/productDetail.js
 var app = getApp();
+const util = require('../../../util/util.js');
 const req = require('../../../util/request.js');
 const config = require('../../../config.js');
 Page({
@@ -18,7 +19,9 @@ Page({
    */
   onLoad: function (options) {
     var self = this;
+    var imgDomain = app.globalData.imgDomain;
     var productId = options.id;
+    console.log(productId);
     var host = config.host;
     var requestData = {};
     requestData.id = productId;
@@ -26,6 +29,7 @@ Page({
       console.log(res);
       if (res && res.data.code == '200') {
         var data = res.data.data;
+        data.cover_url = imgDomain + "card/product/" + data.cover_url;
         console.log(data);
         self.setData({
           productInfo: data
@@ -79,8 +83,19 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function (e) {
+    var self = this;
+    console.log("转发");
+    return {
+      title: self.data.productInfo.product_name,
+      path: '/page/view/productDetail/productDetail?id=' + self.data.productInfo.id,
+      success: function () {
+        console.log("转发成功");
+      },
+      fail: function (err) {
+        console.log("转发失败： " + err);
+      }
+    }
   },
   // 显示咨询页面
   consultShow: function(e) {
@@ -90,11 +105,43 @@ Page({
     console.log(e);
   },
   formSubmit: function(e) {
+    console.log(e);
     var self = this;
+    var data = e.detail.value;
+    if (data.name == null || data.name == "") {
+      self.setData({
+        toastContent: '请输入您的姓名'
+      })
+      self.toast.showToast();
+      return;
+    }
+    if (data.tel == null || data.tel == "") {
+      self.setData({
+        toastContent: '请输入您的手机号码'
+      })
+      self.toast.showToast();
+      return;
+    }
+    var reg = /^1[34578]\d{9}$/;
+    if (reg.test(data.tel) == false) {
+      self.setData({
+        toastContent: '手机号码格式不对'
+      })
+      self.toast.showToast();
+      return;
+    }
+    if (data.content == null) {
+      self.setData({
+        toastContent: '请输入咨询内容'
+      })
+      self.toast.showToast();
+      return;
+    }
     this.setData({
       isShowConsult: false
     });
-    var data = e.detail.value;
+    
+    
     data.salesOpenid = e.detail.target.dataset.openid;
     // data.salesOpenid = app.globalData.openid;
     data.productId = e.detail.target.dataset.id;
@@ -111,5 +158,9 @@ Page({
       console.log(self.toast);
       self.toast.showToast();
     });
+  },
+  share: function() {
+    var productId = this.data.productInfo.id;
+    console.log(productId);
   }
 })
